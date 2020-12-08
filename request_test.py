5b6702a2c54eb15139fd163fe4e2181ed7c72ea9
Grisha <ya.grigory@mail.ru>
2020-12-08 22:50:03 +0500

    Our second commit

--- request_test.py
+++ request_test.py
@@ -1,1 +1,18 @@
+import requests
+import json
+
 print('Hello World')
+
+def get_valutes_list():
+    url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    valutes = list(data['Valute'].values())
+    return valutes
+
+valutes = get_valutes_list()
+print(type(valutes))
+for i in valutes:
+    for j in i.values():
+        print (j,'__',end='')
+    print()
